32271f4dcfb4771da34adf0b2bc693ed19d4c590
BautiColombres <114516945+BautiColombres@users.noreply.github.com>
2025-09-07 17:43:25 -0300

    Back-02-Create Spring Project

---
+++ medi-book-api/src/main/java/com/medibook/api/MediBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.medibook.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediBookApiApplication.class, args);
+	}
+
+}
