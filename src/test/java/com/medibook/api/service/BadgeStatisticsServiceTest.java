6ac0f71e660b3ce6bc9990f844e2ee0a3557b5e4
marianoklecha <114433691+marianoklecha@users.noreply.github.com>
2026-01-22 18:00:32 -0300

    Fix CI/CD part 3

--- src/test/java/com/medibook/api/service/BadgeStatisticsServiceTest.java
+++ src/test/java/com/medibook/api/service/BadgeStatisticsServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.Optional;
@@ -22,6 +23,7 @@ import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @EnableAsync
+@ActiveProfiles("test")
 class BadgeStatisticsServiceTest {
 
     @MockitoBean
